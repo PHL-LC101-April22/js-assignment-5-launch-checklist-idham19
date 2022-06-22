44add09978f41c3e1d944868209f30d754d36b2f
mahdi <mahdibelabbas@gmail.com>
2022-06-22 10:35:21 -0400

    pickPlanet added

--- scriptHelper.js
+++ scriptHelper.js
@@ -4,17 +4,18 @@ require('isomorphic-fetch');
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
    // Here is the HTML formatting for our mission target div.
    const missionTarget =document.getElementById("missionTarget")
+   name =pickPlanet()
    missionTarget.innerHTML=`
    
    <h2>Mission Destination</h2>
    <ol>
-   <li>Name: ${pickPlanet()}</li>
-   <li>Diameter: </li>
+   <li>Name: ${name}</li>
+   <li>Diameter: ${diameter}</li>
    <li>Star: ${star}</li>
-   <li>Distance from Earth: </li>
-   <li>Number of Moons: </li>
+   <li>Distance from Earth:${distance} </li>
+   <li>Number of Moons: ${moons}</li>
    </ol>
-   <img src="">
+   <img src="${imageUrl}">
    
    `
 }
@@ -73,16 +74,17 @@ async function myFetch() {
     let planetsReturned;
 
     planetsReturned = await fetch('https://handlers.education.launchcode.org/static/planets.json')
-    .then((response=>console.log( response.json())))
-
+    .then((response=>( response.json())))
     return planetsReturned;
 }
 
 
 function pickPlanet(planets) {
-    planets=myFetch()
-    return planets
+     planets =['Tatooine','Pern','Saturn/Titan','Mars','K2-18b','Jupiter/Europa']
+    return planets[Math.floor(Math.random()*6)]
+ 
 }
+
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
